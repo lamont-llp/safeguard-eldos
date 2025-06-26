1e84f662549d8062cc22536f4e1fa0bc0e5d504f
Lamont <42653874+lamont-llp@users.noreply.github.com>
2025-06-26 15:21:38 +0200

    Updated firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 
 // Firebase configuration - these will be replaced with actual values
 const firebaseConfig = {
-  apiKey: '${import.meta.env.VITE_FIREBASE_API_KEY}',
-  authDomain: '${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}',
-  projectId: '${import.meta.env.VITE_FIREBASE_PROJECT_ID}',
-  storageBucket: '${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}',
-  messagingSenderId: '${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}',
-  appId: '${import.meta.env.VITE_FIREBASE_APP_ID}',
-  measurementId: '${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID}'
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`,
+  measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
